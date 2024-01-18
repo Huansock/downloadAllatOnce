993bad7305ae9466d129d818bafdf32ab7f920c9
wooseok <huan.sock@gmail.com>
2024-01-18 12:09:43 +0100

    my personal brewapp install

--- brewApps.sh
+++ brewApps.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
+# Chck if user has brew command or not
+if ! type "brew" > /dev/null; then
+    # download brew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Add Homebrew to your PATH 
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/"$(whoami)"/.zprofile
+    eval "$(opt/homebrew/bin/brew shellenv)"
+    else
+        echo ""
+        echo 'you have already brew in your terminal!'
+fi
+
 # download brew and install all brewapps what I want
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && brew install --cask alt-tab alacritty raycast yippy spotify visual-studio-code && brew install htop neovim bat tree
+brew install --cask alt-tab alacritty raycast yippy spotify visual-studio-code && brew install htop neovim bat tree tmux tldr
 
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Add Homebrew to your PATH 
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/"$(whoami)"/.zprofile
-eval "$(opt/homebrew/bin/brew shellenv)"
 
 # Add alias in zsh
 echo "alias py="python3.12"" >> ~/.zshrc

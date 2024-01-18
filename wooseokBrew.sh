f5514ee337c5039e0af88d53837ac89c19869a7e
wooseok <huan.sock@gmail.com>
2024-01-18 12:24:53 +0100

    haaaaaaa

--- wooseokBrew.sh
+++ wooseokBrew.sh
@@ -2,17 +2,19 @@
 
 # Chck if user has brew command or not
 if ! type "brew" > /dev/null; then
+    echo "you don't have brew in your terminal"
     # download brew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
     # Add Homebrew to your PATH 
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/"$(whoami)"/.zprofile
     eval "$(opt/homebrew/bin/brew shellenv)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/"$(whoami)"/.zprofile
     else
         echo ""
         echo 'you have already brew in your terminal!'
 fi
 
+echo "try to install all brew apps"
 # download brew and install all brewapps what I want
 brew install --cask alt-tab alacritty raycast yippy spotify visual-studio-code && brew install htop neovim bat tree tmux tldr
 
